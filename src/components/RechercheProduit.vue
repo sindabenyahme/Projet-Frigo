64e4a052f9403a3dd2f73ddaca68c129362c8e3e
sinda <sinda.ben_yahmed@etud.univ-jfc.fr>
2024-02-25 14:21:30 +0100

    modification

--- src/components/RechercheProduit.vue
+++ src/components/RechercheProduit.vue
@@ -1,13 +1,12 @@
 <script setup>
 import { defineEmits, defineProps } from "vue";
 
-// -- les events émis par le composant
+
 const emit = defineEmits(["searchP"]);
-// -- handler utile si on veut gérer le submit de cette manière
-// <form @submit.prevent="handlerSubmit">
+
 </script>
 <template>
-  <!-- Utilisation des composants Vuetify -->
+  
   <v-form @submit.prevent="$emit('searchP', motcle)" id="recherche">
     <v-text-field v-model="motcle" label="Rechercher ?" id="champ"></v-text-field>
     <v-btn type="submit" id="bouton" color="primary" class="mx-auto d-block btn-addd">Valider</v-btn>
@@ -16,16 +15,16 @@ const emit = defineEmits(["searchP"]);
 
 <style scoped>
 .btn-addd{
-  /* Style du bouton "Valider" */
-  background-color: #007bff; /* Couleur de fond bleue */
-  color: white; /* Couleur du texte blanc */
-  text-align: center; /* Centrage horizontal du texte */
-  padding-left: 50px; /* Ajout de marge à gauche */
-  padding-right: 50px; /* Ajout de marge à droite */
+ 
+  background-color: #1c9ee6;
+  color: white; 
+  text-align: center; 
+  padding-left: 50px; 
+  padding-right: 50px; 
 }
 
 .btn-addd:hover {
-  /* Style du bouton "Valider" au survol */
-  background-color: #0056b3; /* Couleur de fond plus foncée */
+  
+  background-color: #0056b3; 
 }
 </style>
